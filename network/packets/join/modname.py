252026a1067957d272063da2df56e07e9f332b75
HУPΞЯ 사탄 <132200124+HYPER-00@users.noreply.github.com>
2023-07-07 17:41:20 +0100

    Update modname.py

--- network/packets/join/modname.py
+++ network/packets/join/modname.py
@@ -1,3 +1,4 @@
+from core.packet_ids import PacketID, PacketPriority, PacketReliability
 from network.packet_base import Packet
 
 class Packet_PlayerJoinModName(Packet):
@@ -5,6 +6,15 @@ class Packet_PlayerJoinModName(Packet):
         super().__init__()
         self.bitstream.write_ushort(bitstream_version)
         self.bitstream.write_string("deathmatch")
+
+    def get_id(self) -> PacketID:
+        return PacketID.PACKET_ID_MOD_NAME
+    
+    def get_priority(self) -> PacketPriority:
+        return PacketPriority.HIGH
+    
+    def get_reliability(self) -> PacketReliability:
+        return PacketReliability.RELIABLE
 
     def build(self):
         return self.bitstream.get_bytes()
