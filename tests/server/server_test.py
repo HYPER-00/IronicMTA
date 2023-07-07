ab0df07a84b2e61ca4ea156d07aa672bee22c69e
HУPΞЯ 사탄 <132200124+HYPER-00@users.noreply.github.com>
2023-07-07 20:46:12 +0100

    Update server_test.py

--- tests/server/server_test.py
+++ tests/server/server_test.py
@@ -4,8 +4,7 @@ _dir = __file__.split('\\')[:-3]
 if _dir[0].endswith(':'): _dir[0] += '\\'
 sys.path.insert(0, os.path.join(*_dir))
 
-from __init__ import *
+from server import Server
 
-logger = Logger()
-server = Server(logger=logger, settings_file=os.path.realpath("tests\\server\\settings.json"))
+server = Server(settings_file=os.path.realpath("tests\\server\\settings.json"))
 server.start()
