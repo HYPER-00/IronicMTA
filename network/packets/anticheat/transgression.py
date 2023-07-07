2656b982d243438c5994d1555b0fe680ff910d68
HУPΞЯ 사탄 <132200124+HYPER-00@users.noreply.github.com>
2023-07-07 17:51:37 +0100

    Added transgression packet

---
+++ network/packets/anticheat/transgression.py
@@ -0,0 +1,23 @@
+"""
+    Anticheat checks Packet
+"""
+
+from core.packet_ids import PacketID, PacketPriority, PacketReliability
+from network.packet_base import Packet
+
+class Packet_AntiCheatTransgression(Packet):
+    def __init__(self, data) -> None:
+        super().__init__()
+        self.bitstream.refresh(data)
+        # self._message = self.bitstream.read_string()
+
+        # print(self._message)
+
+    def get_id(self) -> PacketID:
+        return PacketID.PACKET_ID_PLAYER_TRANSGRESSION
+    
+    def get_priority(self) -> PacketPriority:
+        return PacketPriority.HIGH
+    
+    def get_reliability(self) -> PacketReliability:
+        return PacketReliability.RELIABLE
