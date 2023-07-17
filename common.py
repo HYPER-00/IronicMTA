e015bd02e69840769783330fa17fe7266fdb9515
HУPΞЯ 사탄 <132200124+HYPER-00@users.noreply.github.com>
2023-07-17 22:29:40 +0100

    Update common.py

--- common.py
+++ common.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 class HttpDownloadTypes:
     """
         Http download types
@@ -24,6 +26,30 @@ class PacketTypes:
         self.ELEMENT_RPC = 1
         self.MAIN_PACKETS = 2
         self.PACKET_ID = 3
+
+class PlayerDisconnectedReason(Enum):
+        NO_REASON = 0
+        INVALID_PASSWORD = 1
+        INVALID_NICKNAME = 2
+        BANNED_SERIAL = 3
+        BANNED_IP = 4
+        BANNED_ACCOUNT = 5
+        VERSION_MISMATCH = 6
+        JOIN_FLOOD = 7
+        INCORRECT_PASSWORD = 8
+        DIFFERENT_BRANCH = 9
+        BAD_VERSION = 10
+        SERVER_NEWER = 11
+        SERVER_OLDER = 12
+        NICK_CLASH = 13
+        ELEMENT_FAILURE = 14
+        GENERAL_REFUSED = 15
+        SERIAL_VERIFICATION = 16
+        CONNECTION_DESYNC = 17
+        BAN = 18
+        KICK = 19
+        CUSTOM = 20
+        SHUTDOWN = 21
 
 
 LOCAL_SERVER_LIST_ASE_PORT = 34219
