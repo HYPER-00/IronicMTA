3641f7230c3059a0e182c6578cdbcb2c205f14c7
HУPΞЯ 사탄 <132200124+HYPER-00@users.noreply.github.com>
2023-07-21 00:30:05 +0100

    Update resources for server test

--- tests/server/server_test.py
+++ tests/server/server_test.py
@@ -6,5 +6,10 @@ sys.path.insert(0, os.path.join(*_dir))
 
 from server import Server
 
-server = Server(settings_file=os.path.realpath("tests\\server\\settings.json"))
+server = Server(main_file=__file__, settings_file=os.path.realpath("PyMTA\\tests\\server\\settings.json"))
 server.start()
+
+print(f"{len(server.getAllResources())} Resources Loaded.")
+for res in server.getAllResources():
+    print(f"Running Resource: {res.getName()}")
+    print(f"Running Resource Path: {res.getCorePath()}")
