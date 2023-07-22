58970e39b5847da24bc2dd756c5eabc76a8436e6
HУPΞЯ 사탄 <132200124+HYPER-00@users.noreply.github.com>
2023-07-22 11:49:27 +0100

    Added server event test

--- tests/server/server_test.py
+++ tests/server/server_test.py
@@ -7,11 +7,14 @@ sys.path.insert(0, os.path.join(*_dir))
 from server import Server
 
 server = Server(main_file=__file__, settings_file="settings.json")
-server.start()
 
 print(f"{server.getTotalResourcesCount()} Resources Loaded.")
 for res in server.getAllResources():
     print(f"Running Resource Path: {res.getCorePath()}")
 
-network = server.getNetwork()
-print(network.getNetRoute())
+@server.event.onServerStart
+def x(server):
+    print("Server Has Been started")
+    print(f"Server Name: {server.getName()}")
+
+server.start()
