c75f75ae8fb593f72cbbdf036a8b33af95609577
HУPΞЯ 사탄 <132200124+HYPER-00@users.noreply.github.com>
2023-07-22 12:50:54 +0100

    Update server event handler

--- event_manager/event.py
+++ event_manager/event.py
@@ -4,11 +4,62 @@ from errors import EventHandlerError
 class EventHandler(object):
     def __init__(self):
         self._global_events = {
-            "onServerStart": None
+            "onServerInitalize": None,
+            "onServerStart": None,
+            "onServerNetworkStart": None,
+            "onMasterServerAnnounce": None,
+            "onHTTPServerStart": None,
+            "onServerSettingsLoad": None
         }
 
+    def onServerInitalize(self, _func):
+        """onServerInitialize
+
+        Args:
+            arg_1 (server): Server Instance
+        """
+        self._global_events["onServerInitalize"] = _func
+
     def onServerStart(self, _func):
+        """onServerStart
+
+        Args:
+            arg_1 (server): Server Instance
+        """        
         self._global_events["onServerStart"] = _func
+    
+    def onServerNetworkStart(self, _func):
+        """onServerNetworkStart
+
+        Args:
+            arg_1 (NetworkWrapper): Server Network Instance
+        """        
+        self._global_events["onServerNetworkStart"] = _func
+    
+    def onMasterServerAnnounce(self, _func):
+        """onMasterServerAnnounce
+
+        Args:
+            arg_1 (server): Server Instance
+        """        
+        self._global_events["onMasterServerAnnounce"] = _func
+
+    def onHTTPServerStart(self, _func):
+        """onHTTPServerStart
+
+        Args:
+            arg_1 (HTTPServer): Server Instance
+            arg_2 (HTTPServer): HTTP Server Instance
+        """        
+        self._global_events["onHTTPServerStart"] = _func
+
+    def onServerSettingsLoad(self, _func):
+        """onServerSettingsLoad
+
+        Args:
+            arg_1 (dict): Server Settings
+        """        
+        self._global_events["onServerSettingsLoad"] = _func
 
     def call(self, event_name: str, *args) -> Any:
         if not event_name in self._global_events.keys():
