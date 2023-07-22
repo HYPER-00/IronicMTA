69532f774a9398aaf653bb6faea739a891b54007
HУPΞЯ 사탄 <132200124+HYPER-00@users.noreply.github.com>
2023-07-22 11:45:40 +0100

    Update event handler

--- event_manager/event.py
+++ event_manager/event.py
@@ -1,5 +1,17 @@
-import discord
-from discord.ext import commands
+from typing import Any
+from errors import EventHandlerError
 
-bot = commands.Bot()
-bot.run()
+class EventHandler(object):
+    def __init__(self):
+        self._global_events = {
+            "onServerStart": None
+        }
+
+    def onServerStart(self, _func):
+        self._global_events["onServerStart"] = _func
+
+    def call(self, event_name: str, *args) -> Any:
+        if not event_name in self._global_events.keys():
+            raise EventHandler(f"{event_name} is not registred!")
+        if self._global_events[event_name]:
+            return self._global_events[event_name](*args)
