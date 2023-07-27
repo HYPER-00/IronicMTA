e930b3a67b2f3118f8662a4941f6eaa91c5ac403
HУPΞЯ 사탄 <132200124+HYPER-00@users.noreply.github.com>
2023-07-27 22:55:35 +0100

    Update disconnected.py

--- network/packets/player/disconnected.py
+++ network/packets/player/disconnected.py
@@ -12,7 +12,7 @@ class Packet_PlayerDisconnected(Packet):
         self.bitstream.refresh(data)
 
         self.disconnected_type = self.bitstream.read_bits(5)
-        print(f"Disconencted Type: {self.disconnected_type}")
+        print(f"Disconencted Type: {PlayerDisconnectedTypes(self.disconnected_type)}")
 
         self.reason = self.bitstream.read_string()
         print(f"Disconencted Reason: '{self.reason}'")
