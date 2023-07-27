396bb21ab35292d51c66fa2847e6099cbb2399be
HУPΞЯ 사탄 <132200124+HYPER-00@users.noreply.github.com>
2023-07-27 22:56:31 +0100

    reformat event manager

--- event_manager/event.py
+++ event_manager/event.py
@@ -1,6 +1,7 @@
 from typing import Any
 from errors import EventHandlerError
 
+
 class EventHandler(object):
     def __init__(self):
         self._global_events = {
@@ -27,23 +28,23 @@ class EventHandler(object):
 
         Args:
             arg_1 (server): Server Instance
-        """        
+        """
         self._global_events["onServerStart"].append(_func)
-    
+
     def onServerNetworkStart(self, _func):
         """onServerNetworkStart
 
         Args:
             arg_1 (NetworkWrapper): Server Network Instance
-        """        
+        """
         self._global_events["onServerNetworkStart"].append(_func)
-    
+
     def onMasterServerAnnounce(self, _func):
         """onMasterServerAnnounce
 
         Args:
             arg_1 (server): Server Instance
-        """        
+        """
         self._global_events["onMasterServerAnnounce"].append(_func)
 
     def onHTTPServerStart(self, _func):
@@ -52,7 +53,7 @@ class EventHandler(object):
         Args:
             arg_1 (HTTPServer): Server Instance
             arg_2 (HTTPServer): HTTP Server Instance
-        """        
+        """
         self._global_events["onHTTPServerStart"].append(_func)
 
     def onServerSettingsLoad(self, _func):
@@ -60,7 +61,7 @@ class EventHandler(object):
 
         Args:
             arg_1 (dict): Server Settings
-        """        
+        """
         self._global_events["onServerSettingsLoad"].append(_func)
 
     def onReceivePacket(self, _func):
@@ -71,7 +72,7 @@ class EventHandler(object):
             arg_2 (PacketID): Packet Id
             arg_3 (int): Player Binary Address
             arg_4 (bytes): Packet Content
-        """        
+        """
         self._global_events["onReceivePacket"].append(_func)
 
     def onServerPortsCheck(self, _func):
@@ -81,7 +82,7 @@ class EventHandler(object):
             arg_1 (Server): Server Instance
             arg_2 (bool): Game Port
             arg_3 (bool): HTTP Port
-        """        
+        """
         self._global_events["onServerPortsCheck"].append(_func)
 
     def call(self, event_name: str, *args) -> Any:
