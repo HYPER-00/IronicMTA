cc05122606b2f8b2963f75c0ad64f29f976ddddf
HУPΞЯ 사탄 <132200124+HYPER-00@users.noreply.github.com>
2023-07-27 23:01:33 +0100

    Reformat resource file class

--- resources/resource_file.py
+++ resources/resource_file.py
@@ -1,17 +1,19 @@
+from errors import ResourceFileError
+from typing import Literal
 from os.path import join, isfile, isdir
 from sys import path
 
 _dir = __file__.split('\\')[:-2]
-if _dir[0].endswith(':'): _dir[0] += '\\'
+if _dir[0].endswith(':'):
+    _dir[0] += '\\'
 _basedir = join(*_dir)
 path.insert(0, _basedir)
 print(_basedir)
 
-from typing import Literal
-from errors import ResourceFileError
 
 class ResourceFile:
     """Resource file, verifies that file"""
+
     def __init__(self, path: str, cache: bool = False) -> None:
         self._path = path
         self._cache = cache
@@ -22,12 +24,13 @@ class ResourceFile:
                                     f"(given path: \"{path}\")"
                                     f"(abstract path: {self._path})")
         if isdir(self._path):
-            raise ResourceFileError(f"\"{self._path}\" is not a file (directory).")
+            raise ResourceFileError(
+                f"\"{self._path}\" is not a file (directory).")
 
     def getPath(self) -> str | None:
         """Returns the file path if exists else returns None"""
         return self._path
-    
+
     def isCachable(self) -> Literal[False] | bool:
         """Is file cachable"""
         return self._cache
