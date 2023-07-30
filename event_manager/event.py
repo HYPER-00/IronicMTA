500478ac5859f82f394b243fd9be6f487ca7290b
HУPΞЯ 사탄 <132200124+HYPER-00@users.noreply.github.com>
2023-07-30 20:38:56 +0100

    Update event handler
    Upate error name from "EventHandler" to "EventHandlerError" :+1:

--- event_manager/event.py
+++ event_manager/event.py
@@ -2,7 +2,7 @@ from typing import Any
 from errors import EventHandlerError
 
 
-class EventHandler(object):
+class EventHandlerError(object):
     def __init__(self):
         self._global_events = {
             "onServerInitalize": [],
@@ -87,7 +87,7 @@ class EventHandler(object):
 
     def call(self, event_name: str, *args) -> Any:
         if not event_name in self._global_events.keys():
-            raise EventHandler(f"{event_name} is not registred!")
+            raise EventHandlerError(f"{event_name} is not registred!")
         if self._global_events[event_name]:
             for _iter_func in self._global_events[event_name]:
                 _iter_func(*args)
