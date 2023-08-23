cb508925c2deb04e177a7fad4ff36292ad148d71
HУPΞЯ 사탄 <132200124+HYPER-00@users.noreply.github.com>
2023-08-23 01:14:34 +0100

    Update server_test.py

--- tests/server/server_test.py
+++ tests/server/server_test.py
@@ -1,18 +1,21 @@
-from pip import main
-import os, sys
+import os
+import sys
 
+# IronicMTA path setup
 _dir = __file__.split('\\')[:-4]
-if _dir[0].endswith(':'): _dir[0] += '\\'
+if _dir[0].endswith(':'):
+    _dir[0] += '\\'
 sys.path.insert(0, os.path.join(*_dir))
 
 import IronicMTA
 
 server = IronicMTA.Server(main_file=__file__, settings_file="settings.json")
-logger = server.getLogger()
+logger = server.getLogger()  # Get server logger
 
 @server.event.onServerStart
-def onstart(server):
+def onstart(s):
     logger.debug(f"{server.getTotalResourcesCount()} Resources Loaded.")
-    logger.debug(f"EVENT-TEST: Server Has Been Started ({server})")
+    logger.debug(f"EVENT-TEST: Server Has Been Started ({s})")
 
 server.start()
+
