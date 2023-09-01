841e8f7b01dc89f34f68a4986e10da22de003853
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2023-09-01 01:57:06 +0100

    Reformat brodcast/master_serverlist.py to be comptatible with pep8

--- brodcast/master_serverlist.py
+++ brodcast/master_serverlist.py
@@ -7,13 +7,13 @@ import requests
 
 class MasterServerListAnnouncement:
     def __init__(self, server):
-        self.logger = server.getLogger()
+        self.logger = server.get_logger()
         self._server = server
-        self.settings = server.getSettings()
-        self._ip, self._port = server.getAddress()
+        self.settings = server.get_settings()
+        self._ip, self._port = server.get_address()
         self.version = self.settings["version"]["recommendedclientversion"]
-        self.extra = f'{server.getMaxPlayers()}_{int(server.isPassworded())}_0_0_0'
-        self.url = f'{MASTER_SERVER_LIST_URL}?g={self._port}&a={self._port + 123}&h={server.getHttpPort()}&v={self.version}&x={self.extra}&ip={self._ip}'
+        self.extra = f'{server.get_max_players()}_{int(server.is_passworded())}_0_0_0'
+        self.url = f'{MASTER_SERVER_LIST_URL}?g={self._port}&a={self._port + 123}&h={server.get_http_port()}&v={self.version}&x={self.extra}&ip={self._ip}'
 
     def start(self):
         self._data = QueryLight(self._server)
