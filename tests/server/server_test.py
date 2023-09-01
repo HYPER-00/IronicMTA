c059e38a7130dea45ec575a3854aa465091aca46
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2023-09-01 02:05:02 +0100

    Reformat server_test.py to be comptatible with pep8

--- tests/server/server_test.py
+++ tests/server/server_test.py
@@ -6,16 +6,17 @@ _dir = __file__.replace("/", "\\").split('\\')[:-4]
 if _dir[0].endswith(':'):
     _dir[0] += '\\'
 sys.path.insert(0, os.path.join(*_dir))
-
 import IronicMTA
 
+
 server = IronicMTA.Server(main_file=__file__, settings_file="settings.json")
-logger = server.getLogger()  # Get server logger
+logger = server.get_logger()  # Get server logger
+
 
 @server.event.onServerStart
 def onstart(s):
-    logger.debug(f"{server.getTotalResourcesCount()} Resources Loaded.")
+    logger.debug(f"{server.get_resources_count()} Resources Loaded.")
     logger.debug(f"EVENT-TEST: Server Has Been Started ({s})")
 
-server.start()
 
+server.start()
