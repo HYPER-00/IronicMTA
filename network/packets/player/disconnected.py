fb27accf4d973629e49485a524650d976464bf41
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2023-09-06 01:48:03 +0100

    Update disconnected packet

--- network/packets/player/disconnected.py
+++ network/packets/player/disconnected.py
@@ -1,28 +1,31 @@
 """
     Player Disconnected Packet
 """
+
+from typing import Optional
 from ....core.packet_ids import PacketID, PacketPriority, PacketReliability
 from ....network.packet_base import Packet
 from ....logger import Logger
 from ....common import PlayerDisconnectedTypes
 
+
 class Packet_PlayerDisconnected(Packet):
-    def __init__(self, data: bytearray, logger: Logger = None) -> None:
+    def __init__(self, data: bytearray, logger: Optional[Logger] = None) -> None:
         super().__init__()
         self.bitstream.refresh(data)
 
         self.disconnected_type = self.bitstream.read_uint32()
-        print(f"Disconencted Type: {self.disconnected_type}")
-
         self.reason = self.bitstream.read_string()
-        print(f"Disconencted Reason: '{self.reason}'")
 
+        if logger:
+            logger.log(f"Disconencted Type: {PlayerDisconnectedTypes(self.disconnected_type)}")
+            logger.log(f"Disconencted Reason: '{self.reason}'")
 
     def get_id(self) -> PacketID:
         return PacketID.PACKET_ID_SERVER_DISCONNECTED
 
     def get_priority(self) -> PacketPriority:
         return PacketPriority.HIGH
-    
+
     def get_reliability(self) -> PacketReliability:
         return PacketReliability.RELIABLE
