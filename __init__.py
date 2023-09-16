92f5dd14f18d1eec09a8a3b1c31de5282d941ef9
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2023-09-16 14:20:54 +0100

    Update main namespace

--- __init__.py
+++ __init__.py
@@ -17,15 +17,57 @@ from .object_manager import *
 from .player_manager import Player
 from .logger import Logger
 from .server import Server
-from .common import *
+
+from .common import (
+    HttpDownloadTypes,
+    PacketTypes,
+    AseVersion,
+    BuildType,
+    QueryTypes,
+    PlayerDisconnectedTypes,
+    LOCAL_SERVER_LIST_ASE_PORT,
+    MASTER_SERVER_LIST_URL,
+    PORT_TESTER_URL,
+)
+
 from .vectors import Vector2, Vector3
-from .errors import *
+from .errors import (
+    MaxMapNameLength,
+    MaxGameTypeLength,
+    MySQLUnkownError,
+    MySQLConnectionDetected,
+    SQLQueryUnkownConditionType,
+    MySQLNoConnection,
+    SQLite3NullPath,
+    SQLite3ConnectionDetected,
+    SQLite3NoConnection,
+    SQLite3UnkownError,
+    SettingsLoadingError,
+    SettingsFileError,
+    InvalidPortNumber,
+    ServerNotRunning,
+    ServerNetworkingError,
+    ServerPasswordError,
+    NetworkWrapperInitError,
+    NetworkWrapperError,
+    ResourceFileError,
+    BitStreamError,
+    EventHandlerError,
+)
 from .brodcast import BrodcastManager, PortChecker
 from .httpserver import HTTPServer
 from .resources import Resource, ResourceFile, ResourceInfo, ResourceLoader
 from .event import ServerEventHandler
 from .core import NetworkWrapper, BitStream, PacketID, PacketPriority, PacketReliability
-from .network.packets import *
+
+from .network.packets import (
+    Packet_PlayerJoinModName,
+    Packet_PlayerJoinData,
+    Packet_PlayerConnectComplete,
+    Packet_PlayerDisconnected,
+    Packet_AntiCheatTransgression,
+)
+
 from .settings import SettingsManager
 from .limits import *
 from .client_manager import Client
