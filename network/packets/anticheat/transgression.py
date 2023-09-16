aa2afd2e12506b5adc20916d908138f64b6dcb84
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2023-09-16 14:33:05 +0100

    Update transgression packet imports

--- network/packets/anticheat/transgression.py
+++ network/packets/anticheat/transgression.py
@@ -2,8 +2,9 @@
     Anticheat checks Packet
 """
 
-from ....core.packet_ids import PacketID, PacketPriority, PacketReliability
-from ....network.packet_base import Packet
+from IronicMTA.core.packet_ids import PacketID, PacketPriority, PacketReliability
+from IronicMTA.network.packet_base import Packet
+
 
 class Packet_AntiCheatTransgression(Packet):
     def __init__(self, data) -> None:
@@ -12,13 +13,11 @@ class Packet_AntiCheatTransgression(Packet):
         self._level = self.bitstream.read_uint32()
         self._message = self.bitstream.read_string()
 
-        print("AntiCheat Message: ", self._message)
-
     def get_id(self) -> PacketID:
         return PacketID.PACKET_ID_PLAYER_TRANSGRESSION
-    
+
     def get_priority(self) -> PacketPriority:
         return PacketPriority.HIGH
-    
+
     def get_reliability(self) -> PacketReliability:
         return PacketReliability.RELIABLE
