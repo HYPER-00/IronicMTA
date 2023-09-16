d7fea719be8f9a111b6333a5fa038bba2a8bc350
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2023-09-16 14:35:55 +0100

    Update mod name packet imports

--- network/packets/join/modname.py
+++ network/packets/join/modname.py
@@ -1,5 +1,6 @@
-from ....core.packet_ids import PacketID, PacketPriority, PacketReliability
-from ....network.packet_base import Packet
+from IronicMTA.core.packet_ids import PacketID, PacketPriority, PacketReliability
+from IronicMTA.network.packet_base import Packet
+
 
 class Packet_PlayerJoinModName(Packet):
     def __init__(self, bitstream_version) -> None:
@@ -9,13 +10,12 @@ class Packet_PlayerJoinModName(Packet):
 
     def get_id(self) -> PacketID:
         return PacketID.PACKET_ID_MOD_NAME
-    
+
     def get_priority(self) -> PacketPriority:
         return PacketPriority.HIGH
-    
+
     def get_reliability(self) -> PacketReliability:
         return PacketReliability.RELIABLE
 
     def build(self):
         return self.bitstream.get_bytes()
-
