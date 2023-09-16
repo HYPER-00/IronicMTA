fce64c45b4e5b2f29069bb832c865f1dcd3b1a10
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2023-09-16 14:22:48 +0100

    Update packet handler imports

--- core/packet_handler/core.py
+++ core/packet_handler/core.py
@@ -1,25 +1,50 @@
-from ctypes import c_char_p, string_at
-from ...network.packets import *
-from ...core.packet_ids import PacketID
-from ...common import BITSTREAM_VERSION
+"""Packet Handler Core"""
+from typing import Any, Tuple
+from IronicMTA.network.packets import Packet_PlayerJoinModName
+from IronicMTA.core.packet_ids import PacketID
+from IronicMTA.common import BITSTREAM_VERSION
 
 
 class PacketHandler(object):
+    """Packet Handler
+
+    Args:
+    -----
+        server (Server): IronicMTA server
+    """
+
     def __init__(self, server) -> None:
         self._server = server
         self._network = server.get_network()
         self._logger = server.get_logger()
-        self._packet = None
+        self._packet: Any
         self._packet_index = -1
 
-    def onrecive(self, packet: int, player: int, packet_content: c_char_p, packet_index: int):
+    def onrecive(
+        self, packet: int, player: int, packet_index: int, packet_content: Tuple[Any]
+    ) -> bool:
+        """On Receive Packet
+
+        Args:
+        -----
+            packet (int): Packet id
+            player (int): Player binary address
+            packet_index (int): Packet index from all packets
+
+        Returns:
+        --------
+            bool: True if the packet was handled successfuly
+        """
         if self._packet_index != packet_index:
             self._packet_index = packet_index
             if packet != 0 and player != 0:
                 self._server.event.call(
-                    "onReceivePacket", self._server, packet, player, bytes())
+                    "onReceivePacket", self._server, packet, player, bytes()
+                )
 
                 self._logger.debug(f"Received {PacketID(packet)}")
+                print("Packet Content: ", packet_content)
+                print("====================\n")
 
                 if packet == PacketID.PACKET_ID_PLAYER_JOIN.value:
                     self._packet = Packet_PlayerJoinModName(BITSTREAM_VERSION)
@@ -42,10 +67,10 @@ class PacketHandler(object):
                     self._network.send(
                         player_binaddr=player,
                         packet_id=self._packet.get_id().value,
-                        bitstream_version=1,  # TODO
+                        bitstream_version=BITSTREAM_VERSION,
                         data=self._packet.build(),
                         priority=self._packet.get_priority(),
-                        reliability=self._packet.get_reliability()
+                        reliability=self._packet.get_reliability(),
                     )
                     return True
         return False
