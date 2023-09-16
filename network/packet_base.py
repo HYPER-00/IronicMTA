0481c0e5c2bdb1871d7bbf5ba2f647f498c21efb
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2023-09-16 14:38:30 +0100

    Update packet base

--- network/packet_base.py
+++ network/packet_base.py
@@ -9,22 +9,17 @@ class Packet(object):
         self.bitstream = BitStream()
 
     def build(self):
+        """Get packet bytes"""
         return self.bitstream.get_bytes()
 
     def get_id(self) -> PacketID:
-        """
-            Get The Packet ID Will be Sent
-        """
-        ...
+        """Get The Packet ID Will be Sent"""
+        return PacketID.name
 
     def get_priority(self) -> PacketPriority:
-        """
-            Get The Packet Priority Will be Sent
-        """
-        ...
+        """Get The Packet Priority Will be Sent"""
+        return PacketPriority.name
 
     def get_reliability(self) -> PacketReliability:
-        """
-            Get The Packet Reliability Will be Sent
-        """
-        ...
+        """Get The Packet Reliability Will be Sent"""
+        return PacketReliability.name
